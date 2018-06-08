e2ad6353beb39e047953c3770868f82ed7b387d6
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-06-08 10:56:45 +0100

    Add BDT info to MC pipipi0 data.

--- python/AGammaD0Tohhpi0/data.py
+++ python/AGammaD0Tohhpi0/data.py
@@ -13,7 +13,7 @@ datapaths = {'Data_2015_pipipi0' : ('DecayTree', os.path.join(datadir, 'data/DaV
              'Data_2015_Kpipi0' : ('DecayTree', os.path.join(datadir, 'data/DaVinciTuples_S24r1_part_Kpipi0.root')),
              'Data_2016_pipipi0_HLT2TIS' : ('DecayTree', os.path.join(datadir, 'data/DaVinciTuples_HLT2TIS.root')),
              'MC_2016_pipipi0_HLT2TIS' : ('DecayTree', os.path.join(datadir, 'mc/DaVinciTuples_MC_Matched.root')),
-             'MC_2016_pipipi0' : ('DecayTree', os.path.join(datadir, 'mc/DaVinciTuples_MC_S28_Matched.root')),
+             'MC_2016_pipipi0' : ('DecayTree', os.path.join(datadir, 'mc/DaVinciTuples_MC_S28_Matched_pipipi0.root')),
              }
 
 datalib = DataLibrary(datapaths, variables, varnames)
---
+++ scripts/data-prep/mergetrees.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../../run \$ANALYSISUTILSROOT/scripts/mergetrees.py \$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_pipipi0.root \$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched.root DecayTree \$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_BDT.root BDTTree
