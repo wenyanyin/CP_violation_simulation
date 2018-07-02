12647bba0d10b9a663dbbcb574083eb1165becc3
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-07-02 15:54:36 +0100

    Add Lewis' Kpipi0 BDT training.

--- scripts/data-prep/addmva.sh
+++ scripts/data-prep/addmva.sh
@@ -13,11 +13,17 @@ mva=BDT
 #inputtree="DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree"
 #outputfile=\$AGAMMAD0TOHHPI0DATADIR/data/DaVinciTuples_S24r1_part_BDT_Kpipi0.root
 
-inputfile=\$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_pipipi0.root
-inputtree=DecayTree
-outputfile=\$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_pipipi0_BDT.root
-outputtree=BDTTree
+#inputfile=\$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_pipipi0.root
+#inputtree=DecayTree
+#outputfile=\$AGAMMAD0TOHHPI0DATADIR/mc/DaVinciTuples_MC_S28_Matched_pipipi0_BDT.root
+#outputtree=BDTTree
 
-../../../run python \$ANALYSISUTILSROOT/python/AnalysisUtils/addmva.py --inputfile ${inputfile/\$/\$} --inputtree "$inputtree" --outputfile $outputfile --outputtree BDTTree --weightsfile \$AGAMMAD0TOHHPI0ROOT/tmva/20180327-Theodora/TMVAClassification_${mva}.weights.xml --weightsvar ${mva} && \
-../../../run \$ANALYSISUTILSROOT/scripts/mergetrees.py tmp.root ${inputfile/\$/\$} $inputtree ${outputfile/\$/\$} $outputtree && \
-../../../run mv tmp.root ${inputfile/\$/\$}
+#../../../run python \$ANALYSISUTILSROOT/python/AnalysisUtils/addmva.py --inputfile ${inputfile/\$/\$} --inputtree "$inputtree" --outputfile $outputfile --outputtree BDTTree --weightsfile \$AGAMMAD0TOHHPI0ROOT/tmva/20180327-Theodora/TMVAClassification_${mva}.weights.xml --weightsvar ${mva} && \
+#../../../run \$ANALYSISUTILSROOT/scripts/mergetrees.py tmp.root ${inputfile/\$/\$} $inputtree ${outputfile/\$/\$} $outputtree && \
+#../../../run mv tmp.root ${inputfile/\$/\$}
+
+inputtree="DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree"
+for inputfile in $(ls $AGAMMAD0TOHHPI0DATADIR/data/2015/*/DaVinciTuples_*.root) ; do
+    outputfile=${inputfile/\.root/_BDT.root}
+    ../../../run python \$ANALYSISUTILSROOT/python/AnalysisUtils/addmva.py --inputfile ${inputfile/\$/\$} --inputtree "$inputtree" --outputfile $outputfile --outputtree BDTTree --weightsfile \$AGAMMAD0TOHHPI0ROOT/tmva/20180327-Theodora/TMVAClassification_${mva}.weights.xml --weightsvar ${mva}
+done
