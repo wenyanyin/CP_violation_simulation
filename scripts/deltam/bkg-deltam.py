03353fb069505bde83874cd99f3d74ff640d38cd
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-07-06 15:32:15 +0100

    Update deltam script.

--- scripts/deltam/bkg-deltam.py
+++ scripts/deltam/bkg-deltam.py
@@ -2,6 +2,8 @@
 
 from AGammaD0Tohhpi0.data import datalib
 import ROOT
+from AnalysisUtils.treeutils import TreeFormula
+
 ROOT.gROOT.SetBatch(True)
 tdata = datalib.Data_2015_Kpipi0()
 tmb = datalib.MiniBias_2015()
@@ -9,11 +11,15 @@ tmb = datalib.MiniBias_2015()
 idata = 0
 c = ROOT.TCanvas()
 h = ROOT.TH1F('h', '', 100, 140, 170)
+
+lab1_P = tuple(TreeFormula(tdata, 'lab1_P' + comp) for comp in 'XYZE')
+piplus_P = tuple(TreeFormula(tmb, 'piplus_P' + comp) for comp in 'XYZE')
+
 for i in xrange(min(tmb.GetEntries(), tdata.GetEntries())) :
     tmb.GetEntry(i)
     tdata.GetEntry(i)
-    pD = ROOT.TLorentzVector(tdata.lab1_PX, tdata.lab1_PY, tdata.lab1_PZ, tdata.lab1_PE)
-    ppi = ROOT.TLorentzVector(tmb.piplus_PX, tmb.piplus_PY, tmb.piplus_PZ, tmb.piplus_PE)
+    pD = ROOT.TLorentzVector(*tuple(p() for p in lab1_P))
+    ppi = ROOT.TLorentzVector(*tuple(p() for p in piplus_P))
     pDst = ROOT.TLorentzVector(pD)
     pDst += ppi
     h.Fill(pDst.M() - pD.M())
