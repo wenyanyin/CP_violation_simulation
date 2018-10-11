867799fe76a8b4370bcc70128a5f6229ef89698e
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-10-11 12:05:25 +0100

    Update data to full 2015 & 2016 samples; add mass window selection.

--- python/AGammaD0Tohhpi0/data.py
+++ python/AGammaD0Tohhpi0/data.py
@@ -3,7 +3,7 @@
 import os, ROOT, pprint, glob
 from AnalysisUtils.data import DataLibrary
 from AGammaD0Tohhpi0.variables import variables
-import Reco16_Charm_MagUp_TupleURLs, Reco16_Charm_MagDown_TupleURLs
+from AGammaD0Tohhpi0.selection import selection_R
 
 datadir = os.environ.get('AGAMMAD0TOHHPI0DATADIR', 
                          '/nfs/lhcb/d2hh01/hhpi0/')
@@ -11,19 +11,27 @@ varnames = ('deltam',)
 
 # All the TTree datasets, the tree names and file names (any number of file names can be given).
 datapaths = {'Data_2015_pipipi0' : ('DecayTree', os.path.join(datadir, 'data/DaVinciTuples_S24r1_part_pipipi0.root')),
-             'Data_2015_Kpipi0' : ('DecayTree', os.path.join(datadir, 'data/DaVinciTuples_S24r1_part_Kpipi0.root')),
              'Data_2016_pipipi0_HLT2TIS' : ('DecayTree', os.path.join(datadir, 'data/DaVinciTuples_HLT2TIS.root')),
              'MC_2016_pipipi0_HLT2TIS' : ('DecayTree', os.path.join(datadir, 'mc/DaVinciTuples_MC_Matched.root')),
              'MC_2016_pipipi0' : ('DecayTree', os.path.join(datadir, 'mc/DaVinciTuples_MC_S28_Matched_pipipi0.root')),
-             'MiniBias_2015' : ('pions_tuple_sel/DecayTree',) + tuple(glob.glob(os.path.join(datadir, '../minibias/2015/*/DVTuples*.root'))),
-             'Data_2016_Kpipi0_MagUp_full' : ('DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree',)\
-                 + tuple(Reco16_Charm_MagUp_TupleURLs.urls),
-             'Data_2016_Kpipi0_MagDown_full' : ('DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree',)\
-                 + tuple(Reco16_Charm_MagDown_TupleURLs.urls),
-             'Data_2016_Kpipi0_MagUp' : ('DecayTree', os.path.join(datadir, 'data/Data_2016_Kpipi0_MagUp.root')),
+             'MiniBias_2015' : ('pions_tuple_sel/DecayTree',) \
+                 + tuple(glob.glob(os.path.join(datadir, '../minibias/2015/*/DVTuples*.root'))),
+             
              'Data_2016_Kpipi0_MagDown' : ('DecayTree', os.path.join(datadir, 'data/Data_2016_Kpipi0_MagDown.root')),
              }
 
-selection = 'BDT > -0.1'
+for mag in 'Up', 'Down' :
+    datapaths['Data_2015_Kpipi0_Mag' + mag] = \
+        {'tree' : 'DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree',
+         'files' : sorted(glob.glob(os.path.join(datadir, 'data/2015/mag{0}/*Data.root'.format(mag.lower())))),
+         'friends' : ('Data_2015_Kpipi0_Mag' + mag + '_Weights',)}
+    datapaths['Data_2015_Kpipi0_Mag' + mag + '_Weights'] = \
+        {'tree' : 'BDTTree',
+         'files' : sorted(glob.glob(os.path.join(datadir, 'data/2015/mag{0}/*Kpipi0_BDT.root'.format(mag.lower()))))}
+    mod2016 = __import__('AGammaD0Tohhpi0.Reco16_Charm_Mag{0}_TupleURLs'.format(mag), fromlist = ['urls'])
+    datapaths['Data_2016_Kpipi0_Mag' + mag + '_full'] = {'tree' : 'DstarD0ToHHPi0_Kpipi0_R_LineTuple/DecayTree',
+                                                         'files' : mod2016.urls}
+    datapaths['Data_2016_Kpipi0_Mag' + mag] = {'tree' : 'DecayTree', 
+                                               'files' : (os.path.join(datadir, 'data/Data_2016_Kpipi0_MagUp.root')),}
 
-datalib = DataLibrary(datapaths, variables, varnames = varnames, selection = selection)
+datalib = DataLibrary(datapaths, variables, varnames = varnames, selection = selection_R)
--- python/AGammaD0Tohhpi0/selection.py
+++ python/AGammaD0Tohhpi0/selection.py
@@ -6,3 +6,11 @@ MC_sel_Dst_R = 'abs(lab0_TRUEID)==413 && abs(lab1_TRUEID)==421 && abs(lab5_TRUEI
 MC_sel_pipi_R = AND(MC_sel_Dst_R, 'abs(lab3_TRUEID)==211 && abs(lab4_TRUEID)==211 && lab0_MCMatch_pipi')
 MC_sel_KK_R = AND(MC_sel_Dst_R, 'abs(lab3_TRUEID)==321 && abs(lab4_TRUEID)==321 && lab0_MCMatch_KK')
 MC_sel_Kpi_R = AND(MC_sel_Dst_R, '((abs(lab3_TRUEID)==321 && abs(lab4_TRUEID)==211) || (abs(lab3_TRUEID)==211 && abs(lab4_TRUEID)==321)) && lab0_MCMatch_pipi')
+
+masswindow_R = (1825, 1905)
+masswindow_R_sel = '{0} < D0_mass && D0_mass < {1}'.format(*masswindow_R)
+
+bdtcut = -0.1
+bdtsel = 'BDT >= ' + str(bdtcut)
+
+selection_R = AND(masswindow_R_sel, bdtsel)
