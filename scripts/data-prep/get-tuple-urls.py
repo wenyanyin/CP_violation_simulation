a00306df93b4cdaeff729fa8aa824e6d62f494fe
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-10-17 14:01:58 +0100

    Update tuple URLs.

--- scripts/data-prep/get-tuple-urls.py
+++ scripts/data-prep/get-tuple-urls.py
@@ -5,6 +5,11 @@ import os
 
 for mag in 'Up', 'Down' :
     mod2016 = __import__('AGammaD0Tohhpi0.Reco16_Charm_Mag{0}_TupleLFNs'.format(mag), fromlist = ['lfns'])
+    urls2016 = __import__('AGammaD0Tohhpi0.Reco16_Charm_Mag{0}_TupleURLs'.format(mag), fromlist = ['urls'])
+    for lfn, urls in urls2016.urls.items() :
+        if urls and urls[0].startswith('file:') :
+            urls2016.urls[lfn] = []
     urls = get_access_urls(mod2016.lfns,
                            outputfile = os.path.join(os.environ['AGAMMAD0TOHHPI0ROOT'], 
-                                                     'python/AGammaD0Tohhpi0/Reco16_Charm_Mag{0}_TupleURLs.py'.format(mag)))
+                                                     'python/AGammaD0Tohhpi0/Reco16_Charm_Mag{0}_TupleURLs.py'.format(mag)),
+                           urls = urls2016.urls)
