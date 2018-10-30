a1b2364c684a3756d0bf6635cae603823ea32634
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2018-10-30 12:31:37 +0000

    Print total lumi.

--- scripts/data-prep/get-lumi.py
+++ scripts/data-prep/get-lumi.py
@@ -4,11 +4,13 @@ from AGammaD0Tohhpi0.data import datalib
 from AnalysisUtils.treeutils import tree_iter
 total = 0.
 for year in '2016', :
+    yeartotal = 0.
     for mag in 'Up', 'Down' :
         t = datalib.get_data('Data_2016_Mag' + mag + '_lumi')
         lumiiter = tree_iter(t, 'IntegratedLuminosity')
         lumi = sum(lumiiter)
-        print year, 'Mag' + mag, ':', '{0:.3f}'.format(lumi/1e6)
+        print year, 'Mag' + mag, ':', '{0:.1f} pb^-1'.format(lumi)
         total += lumi
-
-print 'total:', '{0:.3f}'.format(total/1e6)
+        yeartotal += lumi
+    print year, 'total:', '{0:.1f} pb^-1'.format(yeartotal)
+print 'total:', '{0:.1f} pb^-1'.format(total)
