2cc0cb472c0b59052ca04ee771aeca280714b817
wenyanyin <3020086708@qq.com>
2019-02-21 04:45:09 +0000

    extract number

--- scripts/mint/plot.sh
+++ scripts/mint/plot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #copy plotTime.C file into 10x10 bins, approximate 100 dirs
-echo /nfs/lhcb/malexander01/wenyan/phibin*/ | xargs -n 1 cp -v CP_violation_simulation/scripts/mint/plotTime.C
+echo /nfs/lhcb/malexander01/wenyan/phibin*/ | xargs -n 1 cp -v ~/CP_violation_simulation/scripts/mint/plotTime.C
 cd /nfs/lhcb/malexander01/wenyan/
 #create output file
 touch results.txt
@@ -9,7 +9,13 @@ touch results.txt
 dirnames=${ls phibin*}
 for dir in ${dirnames}
 do
-	root -l $dir/plotTime.C
+	cd $dir
+	root -l plotTime.C >stdout
+	#Extract number of the standard output: AGAMMA
+	sed -n '3p' test | tr -cd "[0-9] ." >> ../results.txt
+	#Extract number of the standard output: AGAMMAERROR
+	sed -n '4p' test | tr -cd "[0-9] ." >> ../results.txt
+	cd ..
 done
 
 #./ampFit < pipipi0.txt >& stdout &
