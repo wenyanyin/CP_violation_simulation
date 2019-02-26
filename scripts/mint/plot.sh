aee6343c0ee0a3666ed42d10082e3146ecf8d477
wenyanyin <3020086708@qq.com>
2019-02-26 22:31:01 +0000

    10x10 bins

--- scripts/mint/plot.sh
+++ scripts/mint/plot.sh
@@ -4,18 +4,30 @@
 echo /nfs/lhcb/malexander01/wenyan/phibin*/ | xargs -n 1 cp -v ~/CP_violation_simulation/scripts/mint/plotTime.C
 cd /nfs/lhcb/malexander01/wenyan/
 #create output file
+rm results.txt
 touch results.txt
 
-dirnames=${ls phibin*}
+dirnames=${ls $path}
+
 for dir in ${dirnames}
 do
 	cd $dir
+	#output the value of phi and qoverp into results.txt
+	#phi
+	tail -n 2 pipipi0.txt | head -n 1 | tr -cd "[0-9] . -" >> ../results.txt
+	#qoverp
+	tail -n 1 pipipi0.txt | tr -cd "[0-9] ." >> ../results.txt
+
 	root -l plotTime.C >stdout
 	#Extract number of the standard output: AGAMMA
-	sed -n '3p' test | tr -cd "[0-9] ." >> ../results.txt
+	sed -n '3p' stdout | tr -cd "[0-9] ." >> ../results.txt
 	#Extract number of the standard output: AGAMMAERROR
-	sed -n '4p' test | tr -cd "[0-9] ." >> ../results.txt
+	sed -n '4p' stdout | tr -cd "[0-9] ." >> ../results.txt
+	echo -e "\n" >> ../results.txt
 	cd ..
 done
 
 #./ampFit < pipipi0.txt >& stdout &
+
+#Extrcat the second last row
+#cat file | sed -n '2p' or cat file |head -n 2|tail -n 1
